9d041c287ea89e7e1426bd6fb76e9a93c1e43a46
claviering <claviering@gmail.com>
2020-06-26 20:29:19 +0800

    filename

--- webpack.config.lib.js
+++ webpack.config.lib.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "flip-number.js",
+    filename: "index.js",
     library: "flipNumber", // 在全局变量中增加一个library变量
     libraryTarget: "umd"
   }
