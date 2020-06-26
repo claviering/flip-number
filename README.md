a9662385bc336ddeebe571b805faf47db046ccb0
claviering <claviering@gmail.com>
2020-06-26 18:08:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# 原生 js 实现 FLIP 效果实现
+
+Demo
+
+![效果](./FLIP.gif)
+
+[codepen](https://codepen.io/claviering/pen/MWKoXMx)
+
+## 参考
+
+> https://zhuanlan.zhihu.com/p/146641652
+
+> https://github.com/sl1673495/flip-animation
+
+> http://sl1673495.gitee.io/flip-animation/
---
+++ webpack.config.lib.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "flip-number.js",
+    library: "flipNumber", // 在全局变量中增加一个library变量
+    libraryTarget: "umd"
+  }
+}
